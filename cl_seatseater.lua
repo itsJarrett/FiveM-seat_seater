52772390103e4ca28e62f349efdb92f93bc8d965
userMacieG <palatim3@gmail.com>
2019-10-11 13:57:19 +0200

    Update cl_seatseater.lua
    Changed `GetPlayerPed(-1)` to `PlayerPedId()` and make code more readable

--- cl_seatseater.lua
+++ cl_seatseater.lua
@@ -1,8 +1,8 @@
 local doors = {
-  {"seat_dside_f", -1},
-  {"seat_pside_f", 0},
-  {"seat_dside_r", 1},
-  {"seat_pside_r", 2}
+	{"seat_dside_f", -1},
+	{"seat_pside_f", 0},
+	{"seat_dside_r", 1},
+	{"seat_pside_r", 2}
 }
 
 function VehicleInFront(ped)
@@ -10,34 +10,45 @@ function VehicleInFront(ped)
     local entityWorld = GetOffsetFromEntityInWorldCoords(ped, 0.0, 5.0, 0.0)
     local rayHandle = CastRayPointToPoint(pos.x, pos.y, pos.z, entityWorld.x, entityWorld.y, entityWorld.z, 10, ped, 0)
     local _, _, _, _, result = GetRaycastResult(rayHandle)
+	
     return result
 end
 
 Citizen.CreateThread(function()
-  while true do
-    Citizen.Wait(0)
-	local ped = GetPlayerPed(-1)
-    if IsControlJustReleased(0, 23) and running ~= true and GetVehiclePedIsIn(ped, false) == 0 then
-      local vehicle = VehicleInFront(ped)
-      running = true
-      if vehicle ~= nil then
-        local plyCoords = GetEntityCoords(ped, false)
-        local doorDistances = {}
-        for k, door in pairs(doors) do
-          local doorBone = GetEntityBoneIndexByName(vehicle, door[1])
-          local doorPos = GetWorldPositionOfEntityBone(vehicle, doorBone)
-          local distance = Vdist(plyCoords.x, plyCoords.y, plyCoords.z, doorPos.x, doorPos.y, doorPos.z)
-          table.insert(doorDistances, distance)
-        end
-        local key, min = 1, doorDistances[1]
-        for k, v in ipairs(doorDistances) do
-          if doorDistances[k] < min then
-            key, min = k, v
-          end
-        end
-        TaskEnterVehicle(ped, vehicle, -1, doors[key][2], 1.5, 1, 0)
-      end
-      running = false
-    end
-  end
+	while true do
+    	Citizen.Wait(0)
+			
+		local ped = PlayerPedId()
+			
+   		if IsControlJustReleased(0, 23) and running ~= true and GetVehiclePedIsIn(ped, false) == 0 then
+      		local vehicle = VehicleInFront(ped)
+				
+      		running = true
+				
+      		if vehicle ~= nil then
+				local plyCoords = GetEntityCoords(ped, false)
+        		local doorDistances = {}
+					
+        		for k, door in pairs(doors) do
+          			local doorBone = GetEntityBoneIndexByName(vehicle, door[1])
+          			local doorPos = GetWorldPositionOfEntityBone(vehicle, doorBone)
+          			local distance = Vdist(plyCoords.x, plyCoords.y, plyCoords.z, doorPos.x, doorPos.y, doorPos.z)
+						
+          			table.insert(doorDistances, distance)
+        		end
+					
+        		local key, min = 1, doorDistances[1]
+					
+        		for k, v in ipairs(doorDistances) do
+          			if doorDistances[k] < min then
+           				key, min = k, v
+          			end
+        		end
+					
+        		TaskEnterVehicle(ped, vehicle, -1, doors[key][2], 1.5, 1, 0)
+     		end
+				
+      		running = false
+    	end
+  	end
 end)
