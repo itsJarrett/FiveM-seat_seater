c090a639b7833faf0bdf8bc7efdf277194573e86
userMacieG <palatim3@gmail.com>
2019-10-11 13:57:56 +0200

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -1,7 +1,7 @@
 resource_manifest_version '679-996150c95a1d251a5c0c7841ab2f0276878334f7'
+
 author 'Slavko Avsenik'
+
 description 'Seat Seater'
 
-client_scripts {
-  'cl_seatseater.lua'
-}
+client_script 'cl_seatseater.lua'
